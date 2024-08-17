7eeae6789c5b14873008f6c48da490bfc0279015
Daniel349167 <duretae@gmail.com>
2024-08-17 08:39:09 -0500

    heroku link

--- src/assets/mixins/auth.js
+++ src/assets/mixins/auth.js
@@ -1,6 +1,7 @@
 export const auth = {
     methods: {
         authBaseUrl() {
+            return 'https://quiet-oasis-06857-7db50e92e151.herokuapp.com'
             // return 'http://192.168.1.57:8000'
             // return 'http://10.0.2.2:8000'
             return 'http://127.0.0.1:8000'
